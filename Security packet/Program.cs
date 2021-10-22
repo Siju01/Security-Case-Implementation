15ac91b75cf76ebbc77a08b4458bf554424f89eb
Siju <63763376+Siju01@users.noreply.github.com>
2021-10-22 12:17:49 +0700

    1

---
+++ Security packet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExpressEncription;
+
+namespace Security
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //RSA Encryption
+
+            //Generate a key pair
+            ExpressEncription.RSAEncription.MakeKey(@"C:\Pens\Semester 5\Pemrograman Jaringan Komputer\Server\public.key", @"C:\Pens\Semester 5\Pemrograman Jaringan Komputer\Server\private.key");
+
+            //Encrypt
+            var plainText = "Hallo";
+            var cypherText = ExpressEncription.RSAEncription.EncryptString(plainText, @"C:\Pens\Semester 5\Pemrograman Jaringan Komputer\Server\public.key");
+
+            //decrypt
+            var decyrptText = ExpressEncription.RSAEncription.DecryptString(cypherText, @"C:\Pens\Semester 5\Pemrograman Jaringan Komputer\Server\private.key");
+
+        }
+    }
+}
